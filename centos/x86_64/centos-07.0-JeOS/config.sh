1a8db66abc98fae22ac7d56ca77e857fc036b41e
Marcus Schäfer <ms@suse.de>
2018-02-20 09:08:46 +0100

    Use generic service method

--- centos/x86_64/centos-07.0-JeOS/config.sh
+++ centos/x86_64/centos-07.0-JeOS/config.sh
@@ -18,7 +18,7 @@ baseMount
 #======================================
 # Activate services
 #--------------------------------------
-suseInsertService dracut_hostonly
+baseInsertService dracut_hostonly
 
 #======================================
 # Setup default target, multi-user
