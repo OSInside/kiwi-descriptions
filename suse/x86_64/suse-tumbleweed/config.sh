15f971a12a63d7a1afc30033327130051ffba06e
Marcus Schäfer <ms@suse.de>
2021-07-06 14:28:11 +0200

    fixed TW example

--- suse/x86_64/suse-tumbleweed/config.sh
+++ suse/x86_64/suse-tumbleweed/config.sh
@@ -40,16 +40,3 @@ suseInsertService sshd
 # Setup default target, multi-user
 #--------------------------------------
 baseSetRunlevel 3
-
-#==========================================
-# remove package docs
-#------------------------------------------
-rm -rf /usr/share/doc/packages/*
-rm -rf /usr/share/doc/manual/*
-rm -rf /opt/kde*
-
-#======================================
-# only basic version of vim is
-# installed; no syntax highlighting
-#--------------------------------------
-sed -i -e's/^syntax on/" syntax on/' /etc/vimrc
