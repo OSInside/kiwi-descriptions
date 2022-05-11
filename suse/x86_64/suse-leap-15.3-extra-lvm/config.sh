378377e0d1d5092fae11fd6c1f60daae66f9df5f
Marcus Schäfer <marcus.schaefer@gmail.com>
2022-05-11 14:58:51 +0200

    Added suse-leap-15.3-extra-lvm
    A system with LVM as add-on not containing the root
    filesystem. The LVM is placed in an extra partition at
    the end of the disk such that it could also be resized.
    The description uses the spare_part feature and since
    the extra LVM partition is last and not root you are
    on your own regarding the resize. However I can imagine
    existing grow-part solutions (e.g cloud-init) can do
    the trick

---
+++ suse/x86_64/suse-leap-15.3-extra-lvm/config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+test -f /.kconfig && . /.kconfig
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Activate services
+#--------------------------------------
+suseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
+
+#======================================
+# Mountpoint for extra nova volume
+#--------------------------------------
+mkdir -p /var/lib/nova
