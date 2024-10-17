d320127790267cdef92536092c330d6f3103a560
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-10-17 21:05:51 +0200

    Add debian-bullseye example

---
+++ debian/x86_64/debian-bullseye/config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+test -f /.kconfig && . /.kconfig
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Activate services
+#--------------------------------------
+baseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
