f92563307967f062042b61f2329435ed6886aa28
Marcus Schäfer <marcus.schaefer@gmail.com>
2024-11-05 11:09:59 +0100

    Added suse-leap-15.6 template

---
+++ suse/x86_64/suse-leap-15.6/config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+test -f /.kconfig && . /.kconfig
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Activate services
+#--------------------------------------
+suseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
